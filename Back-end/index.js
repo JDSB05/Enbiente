b312976f068cb578430f1bd385fad5b444f7f8b6
JDSB05 <jesus2santos06@hotmail.com>
2024-03-03 23:09:38 +0100

    Update dependencies and remove unused code

--- Back-end/index.js
+++ Back-end/index.js
@@ -4,7 +4,6 @@ const express = require('express')
 //const sequelize = require('./config/sequelize')
 const app = express()
 const PORT = process.env.PORT || 4000
-console.log(process.env.DATABASE, process.env.USER, process.env.PASSWORD, process.env.HOST)
 const bodyParser = require('body-parser');
 const passport = require("passport");
 const cors = require('cors');
