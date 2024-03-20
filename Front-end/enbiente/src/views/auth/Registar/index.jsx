74f675cc61132321a9e471d9bbe8a7eb2e4911fe
JDSB05 <jesus2santos06@hotmail.com>
2024-03-20 13:19:39 +0100

    Translate sign in page to Portuguese e  Registo

--- Front-end/enbiente/src/views/auth/Registar/index.jsx
+++ Front-end/enbiente/src/views/auth/Registar/index.jsx
@@ -1,6 +1,11 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-// Chakra imports
+import React, { useState } from "react";
+import illustration from "../../../assets/img/auth/auth.png";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
+import { RiEyeCloseLine } from "react-icons/ri";
+import { useHistory } from "react-router-dom";
+import api from "../../../services/api";
+import { Modal } from "react-bootstrap";
+import { useToast } from '../../../components/toasts/toast';
 import {
   Box,
   Button,
@@ -16,15 +21,20 @@ import {
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
-// Custom components
 import DefaultAuth from "layouts/auth/Default.js";
-// Assets
-import illustration from "../../../assets/img/auth/auth.png";
-import { MdOutlineRemoveRedEye } from "react-icons/md";
-import { RiEyeCloseLine } from "react-icons/ri";
 
 function Registar() {
-  // Chakra color mode
+  const { showSuccessToast, showErrorToast, showMessageToast } = useToast();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [genero, setGenero] = useState("");
+  const [data, setData] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [submitting, setSubmitting] = useState(false);
+  const history = useHistory();
   const textColor = useColorModeValue("navy.700", "white");
   const textColorSecondary = "gray.400";
   const textColorDetails = useColorModeValue("navy.700", "secondaryGray.600");
@@ -32,16 +42,86 @@ function Registar() {
   const brandStars = useColorModeValue("brand.500", "brand.400");
   const googleBg = useColorModeValue("secondaryGray.300", "whiteAlpha.200");
   const googleText = useColorModeValue("navy.700", "white");
-  const googleHover = useColorModeValue(
-    { bg: "gray.200" },
-    { bg: "whiteAlpha.300" }
-  );
-  const googleActive = useColorModeValue(
-    { bg: "secondaryGray.300" },
-    { bg: "whiteAlpha.200" }
-  );
   const [show, setShow] = React.useState(false);
+  
   const handleClick = () => setShow(!show);
+  const handleChangePassword = (event) => {
+    const { value } = event.target;
+    setPassword(value);
+    validatePassword(value);
+  };
+  const handleChangePasswordConfirm = (event) => {
+    const { value } = event.target;
+    setPasswordConfirm(value);
+    validatePassword(value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    if (submitting) {
+      console.log("Ainda a enviar");
+      showMessageToast("Ainda a enviar! O seu pedido está a ser processado.");
+      return;
+    }
+
+    setSubmitting(true);
+
+  
+    if (password !== passwordConfirm) {
+      showErrorToast("As palavras-passe não são iguais. Tente novamente.");
+      setSubmitting(false);
+      return;
+    }
+    if (!name || !email || !password || !passwordConfirm ) {
+      showErrorToast("Preencha todos os campos.");
+      setSubmitting(false);
+      return;
+    }
+  
+    if (errorMessage) {
+      showErrorToast("A palavra-passe não cumpre as regras.");
+      setSubmitting(false);
+      return;
+    }
+  
+    api.post("/utilizador", {
+        nome: name,
+        email: email,
+        password: password,
+        cargo_id: 1,
+        tipo_cliente_id: 1,
+        estado: "inativo",
+        telemovel: "123456789"
+      })
+      .then((user) => {
+        showSuccessToast("Registo feito com sucesso!");
+        history.push("/login");
+      })
+      .catch((err) => {
+        console.log(err);  
+        setSubmitting(false);
+      });
+  };
+
+  const validatePassword = (value) => {
+    const regex = /^(?=.*[a-zA-Z])(?=.*[0-9])[^\s]{6,}$/;
+
+    if (!value) {
+      setErrorMessage("");
+    } else if (!regex.test(value)) {
+      setErrorMessage("A palavra-passe deve conter pelo menos uma letra maiúscula ou minúscula, um número e ter um comprimento mínimo de 6 caracteres, sem permitir espaços em branco.");
+    } else {
+      setErrorMessage("");
+    }
+  };
+
+
+  const handleModalClose = () => {
+    setShowModal(false);
+    history.push("/login");
+  };
+
   return (
     <DefaultAuth illustrationBackground={illustration} image={illustration}>
       <Flex
@@ -88,9 +168,33 @@ function Registar() {
               fontWeight='500'
               color={textColor}
               mb='8px'>
-              Email<Text color={brandStars}>*</Text>
+              Nome
             </FormLabel>
             <Input
+              value={name}
+              onChange={(event) => setName(event.target.value)}
+              variant='auth'
+              fontSize='sm'
+              ms={{ base: "0px", md: "0px" }}
+              type='text'
+              placeholder='Nome'
+              mb='24px'
+              fontWeight='500'
+              size='lg'
+              isrequired
+            />
+            <FormLabel
+              display='flex'
+              ms='4px'
+              fontSize='sm'
+              fontWeight='500'
+              color={textColor}
+              mb='8px'>
+              Email
+            </FormLabel>
+            <Input
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
               isRequired={true}
               variant='auth'
               fontSize='sm'
@@ -100,6 +204,7 @@ function Registar() {
               mb='24px'
               fontWeight='500'
               size='lg'
+              isrequired
             />
             <FormLabel
               ms='4px'
@@ -107,17 +212,19 @@ function Registar() {
               fontWeight='500'
               color={textColor}
               display='flex'>
-              Password<Text color={brandStars}>*</Text>
+              Palavra-Passe
             </FormLabel>
             <InputGroup size='md'>
               <Input
-                isRequired={true}
+                value={password}
+                onChange={handleChangePassword}
                 fontSize='sm'
                 placeholder='Minimo 8 caracteres'
                 mb='24px'
                 size='lg'
                 type={show ? "text" : "password"}
                 variant='auth'
+                isrequired
               />
               <InputRightElement display='flex' alignItems='center' mt='4px'>
                 <Icon
@@ -128,18 +235,38 @@ function Registar() {
                 />
               </InputRightElement>
             </InputGroup>
-            <Flex justifyContent='space-between' align='center' mb='24px'>
-              <NavLink to='/auth/signin'>
-                <Text
-                  color={textColorBrand}
-                  fontSize='sm'
-                  w='200px'
-                  fontWeight='500'>
-                  Já tem uma conta? Faça login!
-                </Text>
-              </NavLink>
-            </Flex>
+            <FormLabel
+              ms='4px'
+              fontSize='sm'
+              fontWeight='500'
+              color={textColor}
+              display='flex'>
+              Confirmar palavra-Passe
+            </FormLabel>
+            <InputGroup size='md'>
+              <Input
+                value={passwordConfirm}
+                onChange={handleChangePasswordConfirm}
+                fontSize='sm'
+                placeholder='Minimo 8 caracteres'
+                mb='24px'
+                size='lg'
+                type={show ? "text" : "password"}
+                variant='auth'
+                isrequired
+              />
+              <InputRightElement display='flex' alignItems='center' mt='4px'>
+                <Icon
+                  color={textColorSecondary}
+                  _hover={{ cursor: "pointer" }}
+                  as={show ? RiEyeCloseLine : MdOutlineRemoveRedEye}
+                  onClick={handleClick}
+                />
+              </InputRightElement>
+            </InputGroup>
+            {/* Other fields */}
             <Button
+              onClick={handleSubmit}
               fontSize='sm'
               variant='brand'
               fontWeight='500'
@@ -149,29 +276,24 @@ function Registar() {
               Criar conta
             </Button>
           </FormControl>
-          <Flex
-            flexDirection='column'
-            justifyContent='center'
-            alignItems='start'
-            maxW='100%'
-            mt='0px'>
-            <Text color={textColorDetails} fontWeight='400' fontSize='14px'>
-              Já tem uma conta?
-              <NavLink to='/auth/sign-up'>
-                <Text
-                  color={textColorBrand}
-                  as='span'
-                  ms='5px'
-                  fontWeight='500'>
-                  Faça login!
-                </Text>
-              </NavLink>
-            </Text>
-          </Flex>
         </Flex>
       </Flex>
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Registo feito</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Verifique a conta, acedendo ao link que foi enviado para: {email}</p>
+          </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleModalClose}>
+            Ok
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </DefaultAuth>
   );
 }
 
 export default Registar;
+
